2a39cac37a1bd722916489d63823f7d63d9b6a49
zientekb1 <bjzientek@gmail.com>
2023-06-28 20:56:29 -0500

    add_income button now goes to user_input_info.java and the return button on user_input_info takes user back to the main_activity

--- app/src/main/java/com/example/budget_tracking_app_1/MainActivity.java
+++ app/src/main/java/com/example/budget_tracking_app_1/MainActivity.java
@@ -2,25 +2,24 @@ package com.example.budget_tracking_app_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button add_income_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.Add_Income);
-        button.setOnClickListener(new View.OnClickListener(){
+        add_income_button = findViewById(R.id.Add_Income);
+        add_income_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "The add income button has been clicked", Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, user_input_info.class);
+                startActivity(intent);
             }
-
         });
     }
 }
--- app/src/main/java/com/example/budget_tracking_app_1/user_input_info.java
+++ app/src/main/java/com/example/budget_tracking_app_1/user_input_info.java
@@ -2,13 +2,24 @@ package com.example.budget_tracking_app_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class user_input_info extends AppCompatActivity {
-
+    Button return_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input_info);
+        return_button = findViewById(R.id.Return_button);
+        return_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(user_input_info.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
