f010440709aa2b1aed9c605e12de0865e94ca706
zientekb1 <bjzientek@gmail.com>
2023-06-29 19:16:55 -0500

    Is able to take things inputted from user_input_info into a ListArray onto MainActivity however the list doesn't expand when adding multiple elements. The Amount Saved value does work as intended.

--- app/src/main/java/com/example/budget_tracking_app_1/MainActivity.java
+++ app/src/main/java/com/example/budget_tracking_app_1/MainActivity.java
@@ -3,23 +3,76 @@ package com.example.budget_tracking_app_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> addArray = new ArrayList<String>();
+    ListView show;
     Button add_income_button;
+    TextView amount_saved_num;
+    double amount_total = 0.0;
+    SharedPreferences sharedPreferences;
+    public static final String AMOUNT_TOTAL_KEY = "amount_total";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        show = findViewById(R.id.Name_list);
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        // Retrieve the value of amount_total from SharedPreferences
+        amount_total = sharedPreferences.getFloat(AMOUNT_TOTAL_KEY, 0.0f);
+        amount_saved_num = findViewById(R.id.amount_saved_num);
+        amount_saved_num.setText(String.valueOf(amount_total));
+
         add_income_button = findViewById(R.id.Add_Budget_or_Expense);
         add_income_button.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, user_input_info.class);
+                intent.putExtra("amount_total", amount_total);
                 startActivity(intent);
             }
         });
+        // Retrieve the values from the Intent
+        Intent intent = getIntent();
+        if (intent != null) {
+            String name = intent.getStringExtra("name");
+            double income = intent.getDoubleExtra("income", 0.0);
+            String date = intent.getStringExtra("date");
+
+            // Add name, income, and date to the addArray ArrayList
+            addArray.add(name + ": " + income + " (" + date + ")");
+
+            CustomArrayAdapter adapter = new CustomArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, addArray);
+            show.setAdapter(adapter);
+        }
+    }
+
+    private class CustomArrayAdapter extends ArrayAdapter<String> {
+        public CustomArrayAdapter(MainActivity context, int resource, ArrayList<String> items) {
+            super(context, resource, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, android.view.ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+
+            // Set the text color to #B8E1DD
+            TextView textView = (TextView) view.findViewById(android.R.id.text1);
+            textView.setTextColor(Color.parseColor("#B8E1DD"));
+
+            return view;
+        }
     }
 }
--- app/src/main/java/com/example/budget_tracking_app_1/user_input_info.java
+++ app/src/main/java/com/example/budget_tracking_app_1/user_input_info.java
@@ -4,23 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.util.Calendar;
 
 public class user_input_info extends AppCompatActivity {
-    EditText date_edit_text;
+    EditText date_edit_text, name_text, income_text;
     DatePickerDialog datePickerDialog;
-    Button return_button;
+    Button return_button, enter_button;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    double amount_total = 0.0;
+    SharedPreferences sharedPreferences;
+    public static final String AMOUNT_TOTAL_KEY = "amount_total";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input_info);
-        date_edit_text = findViewById(R.id.Date_editText);
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        // Calendar date stuff
+        date_edit_text = findViewById(R.id.date_edit_text);
         Calendar calendar = Calendar.getInstance();
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
         final int year = calendar.get(Calendar.YEAR);
@@ -31,14 +43,14 @@ public class user_input_info extends AppCompatActivity {
                 datePickerDialog = new DatePickerDialog(user_input_info.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        date_edit_text.setText((month+1)+"/"+dayOfMonth+"/"+year);
+                        date_edit_text.setText((month + 1) + "/" + dayOfMonth + "/" + year);
                     }
-                },year, month, day);
+                }, year, month, day);
                 datePickerDialog.show();
             }
         });
 
-
+        // return button stuff
         return_button = findViewById(R.id.Return_button);
         return_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,5 +59,46 @@ public class user_input_info extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // enter button stuff
+        name_text = findViewById(R.id.name_edit_text);
+        income_text = findViewById(R.id.Income_edit_text);
+        date_edit_text = findViewById(R.id.date_edit_text);
+        enter_button = findViewById(R.id.Enter_button);
+        radioGroup = findViewById(R.id.radioGroup);
+        enter_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = name_text.getText().toString();
+                double income = Double.parseDouble(income_text.getText().toString());
+                String date = date_edit_text.getText().toString();
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioId);
+
+                amount_total = sharedPreferences.getFloat(AMOUNT_TOTAL_KEY, 0.0f);
+
+                if (radioId == R.id.Expense_button) {
+                    amount_total -= income;
+                } else if (radioId == R.id.Income_button) {
+                    amount_total += income;
+                } else {
+                    amount_total += 5;
+                }
+
+                // Update the value of amount_total in SharedPreferences
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putFloat(AMOUNT_TOTAL_KEY, (float) amount_total);
+                editor.apply();
+
+                // Create an Intent to return the data to MainActivity
+                Intent intent = new Intent(user_input_info.this, MainActivity.class);
+                intent.putExtra("name", name);
+                intent.putExtra("income", income);
+                intent.putExtra("date", date);
+                intent.putExtra("amount_total", amount_total);
+                startActivity(intent);
+            }
+        });
+
     }
 }
